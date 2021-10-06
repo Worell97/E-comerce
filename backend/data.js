864e8563ad214936889887f72632b03c6cf5484e
Oscar Worell <worell-oscar@hotmail.com.br>
2021-10-05 21:37:48 -0300

    create server applications

---
+++ backend/data.js
@@ -0,0 +1,44 @@
+const data = {
+    products: [{
+        _id: '1',
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/assets/d1.jpg',
+        price: 50,
+        brand: 'Nike',
+        rating: 5,
+        numReviews: 3
+    },
+    {
+        _id: '2',
+        name: 'Fit Shirt',
+        category: 'Shirts',
+        image: '/assets/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4,
+        numReviews: 7
+    },
+    {
+        _id: '3',
+        name: 'Best Shirt',
+        category: 'Pants',
+        image: '/assets/d1.jpg',
+        price: 25,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 30
+    },
+    {
+        _id: '4',
+        name: 'Best Shirt',
+        category: 'Pants',
+        image: '/assets/d1.jpg',
+        price: 25,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 30
+    }]
+};
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import data from './data';
+
+const app = express();
+const port = 5000;
+
+app.get("/api/products", (req, res) => {
+    res.send(data.products);
+});
+
+app.listen(port, () => { console.log("Server started at http://localhost:5000") });
